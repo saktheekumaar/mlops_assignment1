a4f08470399800b0ca0e1fd3535e31fd4af6cc7f
sakthee kumaar <sakthirathnam.k@gmail.com>
2024-07-29 10:04:24 +0530

    update iris file to keep less characters each line

--- iris.py
+++ iris.py
@@ -11,7 +11,9 @@ X = iris.data
 y = iris.target
 
 # Split the dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+    )
 
 # Train a Logistic Regression model
 model = LogisticRegression(max_iter=200)
